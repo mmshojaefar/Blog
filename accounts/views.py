c9da22da8b599f6553ad250489fe98aebd2186c2
mmshojaefar <mmshojaefar@aut.ac.ir>
2021-03-01 14:17:01 +0330

    authentication templates edited!

--- accounts/views.py
+++ accounts/views.py
@@ -2,19 +2,3 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
-
-def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)
-            messages.success(request, 'رمزعبور شما با موفقیت تغییر کرد!')
-            return redirect('change_password')
-        else:
-            messages.error(request, 'للطفا خطاهای زیر را تصحیح کنیدً!')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'accounts/change_password.html', {
-        'form': form
-    })
