9d597d78a531b8befd6b18b619de29eeb88e8bd7
mmshojaefar <mmshojaefar@aut.ac.ir>
2021-04-10 03:37:39 +0430

    Change font and add tag to show number in persian

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,14 @@ from blog.models import Comment_rating, Post_rating, Comment
 
 register = template.Library()
 
+@register.filter(name='persian_int')
+def persian_int(english_int):
+    persian_nums = {'0':'۰', '1':'۱', '2':'۲', '3':'۳', '4':'۴', '5':'۵', '6':'۶', '7':'۷', '8':'۸', '9':'۹'}
+    number = str(english_int)
+    persian_dict = number.maketrans(persian_nums)
+    result = number.translate(persian_dict)
+    return result
+
 @register.filter(name='likes')
 def likes(cm):
     return Comment_rating.objects.filter(positive=True, comment=cm).count()
